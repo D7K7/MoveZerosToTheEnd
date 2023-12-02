79b8708d89b24174fba33d2785def95a335beba0
Dakota Johnson <47164434+D7K7@users.noreply.github.com>
2023-12-02 13:41:27 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,21 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
-
 public class Kata
 {
   public static int[] MoveZeroes(int[] arr)
   {
-    List<int> arrList = arr.ToList();
+      int[] newArray = new int[arr.Length];
 
-    for (int i = 0; i < arr.Length; i++)
-    {
-        //Check if it's a zero AND if there's any numbers left in the list.
-        if (arr[i] == 0 && arrList.Any(x => x > 0))
-        {
-            arrList.Remove(arr[i]);
-            arrList.Add(0);
-        }
+      int newIndex = 0;
+      
+      //No need for Linq or Lists<>, just loop through the array
+      //and assign the same values at new indices.
+      for (int i = 0; i < arr.Length; i++)
+      {
+          if (arr[i] != 0)
+          {
+              newArray[newIndex] = arr[i];
+              newIndex++;
+          }          
+      }
+      return newArray;  
     }
-
-    return arrList.ToArray();
-  }
 }
